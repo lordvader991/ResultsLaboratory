5ec63734b42fd154bb8e4fea3a789bd7aca054a4
Jose_Maria <josemariazapataq2@gmail.com>
2025-06-11 19:18:08 -0400

    Autorization

--- Program.cs
+++ Program.cs
@@ -48,3 +48,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+//algo
--- obj/Debug/net8.0/ResultsService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ResultsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResultsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562130f635762dd02214c0e350b210b8d51fdfc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3775ac1f65e48265d8f86801f5eb869e44d0f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResultsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResultsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
